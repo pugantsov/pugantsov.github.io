eee3eef03f6c450e9ac8af6da13cdd950988bd0d
Alexander Pugantsov <a.pugantsov@icloud.com>
2026-09-26 12:05:41 +0200

    Revise bio and research interests in about.js
    Updated bio section to reflect current research focus and past positions.

--- data/about.js
+++ data/about.js
@@ -1,8 +1,8 @@
 export const ABOUT = {
   bio: [
-    "I am a machine learning researcher working on quantification learning, dataset shift, transfer learning, and the evaluation of machine learning systems under changing data distributions.",
-    "From 2024 to 2026, I was a Postdoctoral Researcher at the [University of Padua](https://www.unipd.it/en/), where I worked on the QuaDaSh project on quantification under dataset shift. I received my PhD in Computing Science from the [University of Glasgow](https://www.gla.ac.uk/schools/computing/), where my research focused on transfer learning, domain transferability, and task similarity.",
-    "More broadly, I am interested in understanding how machine learning systems behave when the data encountered at deployment differs from the data on which they were developed, including questions of robustness, privacy, security, and fairness."
+    "My research focuses on quantification learning, dataset shift, transfer learning, and the evaluation of machine learning systems under changing data distributions.",
+    "From 2024 to 2026, I was a Postdoctoral Researcher in the [Artificial Intelligence, Machine Learning, and Control](https://amco.dei.unipd.it/) group at the [University of Padua](https://www.dei.unipd.it/home-page), working with Professor [Gian Antonio Susto](https://www.unipd.it/contatti/gianantonio.susto) on the QuaDaSh project (*Quantification in the Context of Dataset Shift*). My work there focused on quantification under distributional change, including robustness, population-level inference, fairness, and the privacy and security implications of aggregate prevalence estimation.",
+    "I received my PhD in Computing Science from the [University of Glasgow](https://www.gla.ac.uk/schools/computing/) under the supervision of Dr. [Richard McCreadie](https://www.gla.ac.uk/schools/computing/staff/richardmccreadie/) and Dr. [Jeff Dalton](https://people.inf.ed.ac.uk/Jeff_Dalton.html). My doctoral research investigated transfer learning, domain transferability, task similarity, and methods for predicting when knowledge learned from one task or distribution will transfer effectively to another."
   ],
   researchInterests: [
     "Quantification learning and class-prevalence estimation",
